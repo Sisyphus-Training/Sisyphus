8bbfef3dd05e6d357db2faa758a8047cfa51393d
Victory Chidera Ezeokafor <victoryezeokafor@gmail.com>
2025-03-26 17:52:54 +0100

    feat: implement Fixed Floating Action Button

--- sisyphus/src/app/page.tsx
+++ sisyphus/src/app/page.tsx
@@ -1,12 +1,10 @@
 import Image from "next/image";
 import FabMenu from "@/components/fixedFloatingActionButton";
-import HeroSection from "@/components/Hero";
 
 export default function Home() {
   return (
     <div>
       <h1>Home</h1>
-      <HeroSection />
       <FabMenu />
     </div>
   );
