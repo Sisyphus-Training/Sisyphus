3b2a20ac3291bfe6ddc61fbd8d636890826e3a9b
Victory Chidera Ezeokafor <137406385+viktrified@users.noreply.github.com>
2025-04-03 22:54:10 +0100

    Feat victory (#11)
    * feat: implement Fixed Floating Action Button
    
    * feat: implement Hero Section, issue #9
    
    * feat: implement Fixed Floating Action Button
    
    * fix: create circular button. add label when hovered on, change color and centralised inner buttons. issue#7
    
    * fix: add lable and change menu color to application theme. issue #7

--- sisyphus/src/app/page.tsx
+++ sisyphus/src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import FabMenu from "@/components/fixedFloatingActionButton";
 
 export default function Home() {
   return (
     <div>
       <h1>Home</h1>
+      <FabMenu />
     </div>
   );
 }
